6840eb1277407a8ebc843fd95a46477435ffd0f8
Alan Dias Dos Santos <Alandiasdos598@gmail.com>
2025-11-21 19:41:07 -0300

    feature classe menu atualizada

--- src/controller/ContaController.ts
+++ src/controller/ContaController.ts
@@ -7,21 +7,32 @@ export class ContaController implements ContaRepository {
     private listaContas: Array<Conta> = new Array<Conta>();
     numero: number = 0 ;
 
+    procurarPorNumero(numero: number): void {
+      let buscaConta = this.buscarNoArray(numero);
+            
+            if(buscaConta != null) {
+             buscaConta.visualizar();
+            }else 
+                console.log(colors.fg.red, "\nA Conta numero: " + numero + "não foi encontrada!", colors.reset)
+}
+    
     listarTodas(): void {
         for (let conta of this.listaContas){
             conta.visualizar()
         }
-    }
-
-    procurarPorNumero(numero: number): void {
-        throw new Error("Method not implemented.");
     }
     cadastrar(conta: Conta): void {
         this.listaContas.push(conta);
         console.log(colors.fg.green, `\nA Conta número: ` + conta.numero + "Foi criada com sucesso!", colors.reset);
     }
     atualizar(conta: Conta): void {
-        throw new Error("Method not implemented.");
+        let buscaConta = this.buscarNoArray(conta.numero);
+
+        if(buscaConta != null){
+            this.listaContas[this.listaContas.indexOf(buscaConta)] = conta;
+            console.log(colors.fg.green, "\nA conta numero: " + conta.numero + "foi atualizada com sucesso!", colors.reset);
+        }else 
+            console.log(colors.fg.red, "\nA conta número: " + conta.numero + "não foi encontrada!", colors.reset);
     }
     deletar(numero: number): void {
         throw new Error("Method not implemented.");
@@ -42,5 +53,15 @@ export class ContaController implements ContaRepository {
     public gerarNumero(): number {
         return ++ this.numero;
     }
+
+
+    /*Checa se uma Conta existe*/
+public buscarNoArray (numero: number): Conta | null {
+for (let conta of this.listaContas) {
+if (conta.numero === numero)
+return conta;
+}
+return null;
+}
     
 }
